0f9aaffc611dea2ae73766a073eb9b11aa5b0628
rhit-SlaterE <slatere@rose-hulman.edu>
2021-04-14 22:24:43 -0400

    removing the laggything to make resume work

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,17 @@
-try{
+// try{
 
-var controller = new ScrollMagic.Controller(); // build scenes
-var revealElements = document.getElementsByClassName("digit");
-for (var i = 0; i < revealElements.length; i++) { // create a scene for each element
-    new ScrollMagic.Scene({
-            triggerElement: revealElements[i], // y value not modified, so we can use element as trigger as well
-            offset: 10, // start a little later
-            triggerHook: 1,
-        })
-        .setClassToggle(revealElements[i], "visible") // add class toggle
-        // .addIndicators({name: "digit " + (i+1) }) // add indicators (requires plugin)
-        .addTo(controller);
-}
-}catch(error){
-    console.log("could not load scrollmagic");
-}
+// var controller = new ScrollMagic.Controller(); // build scenes
+// var revealElements = document.getElementsByClassName("digit");
+// for (var i = 0; i < revealElements.length; i++) { // create a scene for each element
+//     new ScrollMagic.Scene({
+//             triggerElement: revealElements[i], // y value not modified, so we can use element as trigger as well
+//             offset: 10, // start a little later
+//             triggerHook: 1,
+//         })
+//         .setClassToggle(revealElements[i], "visible") // add class toggle
+//         // .addIndicators({name: "digit " + (i+1) }) // add indicators (requires plugin)
+//         .addTo(controller);
+// }
+// }catch(error){
+//     console.log("could not load scrollmagic");
+// }
